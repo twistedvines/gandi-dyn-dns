1cb711e69842461587a60552e07f7c037b2e42a1
Harry Owen <harry@twistedvin.es>
2017-11-30 22:06:56 +0000

    Create DNS record

--- update-dns.sh
+++ update-dns.sh
@@ -15,3 +15,24 @@ get_zone_uuid() {
   local zone="$1"
   echo "$zone" | jq '.uuid'
 }
+
+create_dns_record() {
+  local api_key="$1"
+  local domain="$2"
+  local ip_address="$3"
+  local zone_file_uuid="$4"
+
+  local payload="$( \
+    jq ".rrset_name = \"$domain\" |"`
+      `'.rrset_type = "A" |'`
+      `'.rrset_ttl = 3600 |'`
+      `"rrset_values = [\"$ip_address\"]"
+  )"
+
+  local result="$(curl -s \
+    -X PUT \
+    -H 'Content-Type: application/json' \
+    -H "X-Api-Key: $api_key" \
+    -d "$payload" \
+    "https://dns.api.gandi.net/api/v5/zones/${zone_file_uuid}/records")"
+}
