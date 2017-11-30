8093c278734d2a7a924602ca105a5eff68e95c5e
Harry Owen <harry@twistedvin.es>
2017-11-30 22:06:04 +0000

    Get zone from zones based on name

--- update-dns.sh
+++ update-dns.sh
@@ -4,3 +4,9 @@ get_zones() {
   local api_key="$1"
   curl -s -H "X-Api-Key: $api_key" https://dns.api.gandi.net/api/v5/zones
 }
+
+get_zone() {
+  local name="$1"
+  local zones="$2"
+  echo "$zones" | jq ".[] | select(name == $name)"
+}
